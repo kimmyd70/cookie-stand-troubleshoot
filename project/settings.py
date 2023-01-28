aa3184c01f5592e2c9277e1d68431b339352bb1b
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2023-01-28 15:27:59 -0800

    First Commit

--- project/settings.py
+++ project/settings.py
@@ -57,17 +57,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd party
-    "rest_framework",
-    "corsheaders",
+    'rest_framework',
+    'corsheaders',
     # local
-    "accounts",
-    "things",
+    'accounts',
+    'cookie_stands',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
